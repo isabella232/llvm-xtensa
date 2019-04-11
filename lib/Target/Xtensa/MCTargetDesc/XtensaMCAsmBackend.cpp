8cd406a57262c1a6bed17b4eee54c4038aa9be96
Andrei Safronov <safronov@espressif.com>
2019-04-11 11:14:26 +0300

    Corrected fixups processing in xtensa asssmbler backend

--- lib/Target/Xtensa/MCTargetDesc/XtensaMCAsmBackend.cpp
+++ lib/Target/Xtensa/MCTargetDesc/XtensaMCAsmBackend.cpp
@@ -10,6 +10,8 @@
 #include "MCTargetDesc/XtensaMCFixupKinds.h"
 #include "MCTargetDesc/XtensaMCTargetDesc.h"
 #include "llvm/MC/MCAsmBackend.h"
+#include "llvm/MC/MCAssembler.h"
+#include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCELFObjectWriter.h"
 #include "llvm/MC/MCFixupKindInfo.h"
 #include "llvm/MC/MCInst.h"
@@ -51,14 +53,88 @@ public:
 
 const MCFixupKindInfo &
 XtensaMCAsmBackend::getFixupKindInfo(MCFixupKind Kind) const {
-  assert((Kind < FirstTargetFixupKind) && "Invalid kind!");
-  return MCAsmBackend::getFixupKindInfo(Kind);
+  const static MCFixupKindInfo Infos[Xtensa::NumTargetFixupKinds] = {
+      // name                    offset bits  flags
+      {"fixup_xtensa_branch_6", 0, 6, MCFixupKindInfo::FKF_IsPCRel},
+      {"fixup_xtensa_branch_8", 0, 8, MCFixupKindInfo::FKF_IsPCRel},
+      {"fixup_xtensa_jump_18", 0, 18, MCFixupKindInfo::FKF_IsPCRel},
+      {"fixup_xtensa_call_18", 0, 18, MCFixupKindInfo::FKF_IsPCRel},
+      {"fixup_xtensa_l32r_16", 0, 16, MCFixupKindInfo::FKF_IsPCRel},
+  };
+
+  assert(unsigned(Kind - FirstTargetFixupKind) < getNumFixupKinds() &&
+         "Invalid kind!");
+  return Infos[Kind - FirstTargetFixupKind];
+}
+
+static uint64_t adjustFixupValue(const MCFixup &Fixup, uint64_t Value,
+                                 MCContext &Ctx) {
+  unsigned Kind = Fixup.getKind();
+  switch (Kind) {
+  default:
+    llvm_unreachable("Unknown fixup kind!");
+  case FK_Data_1:
+  case FK_Data_2:
+  case FK_Data_4:
+  case FK_Data_8:
+    return Value;
+  case Xtensa::fixup_xtensa_branch_6: {
+    if (!isInt<6>(Value))
+      Ctx.reportError(Fixup.getLoc(), "fixup value out of range");
+    unsigned Hi2 = (Value >> 4) & 0x3;
+    unsigned Lo4 = (Value)&0xf;
+    return (Hi2 << 4) | (Lo4 << 12);
+  }
+  case Xtensa::fixup_xtensa_branch_8:
+    if (!isInt<8>(Value))
+      Ctx.reportError(Fixup.getLoc(), "fixup value out of range");
+    return (Value & 0x3f) << 16;
+  case Xtensa::fixup_xtensa_jump_18:
+    if (!isInt<18>(Value))
+      Ctx.reportError(Fixup.getLoc(), "fixup value out of range");
+    return (Value & 0x3ffff) << 6;
+  case Xtensa::fixup_xtensa_call_18:
+    if (!isInt<20>(Value))
+      Ctx.reportError(Fixup.getLoc(), "fixup value out of range");
+    if (Value & 0x3)
+      Ctx.reportError(Fixup.getLoc(), "fixup value must be 4-byte aligned");
+    return (Value & 0x3ffff) << 6;
+  case Xtensa::fixup_xtensa_l32r_16:
+    if (!isInt<18>(Value))
+      Ctx.reportError(Fixup.getLoc(), "fixup value out of range");
+    if (Value & 0x3)
+      Ctx.reportError(Fixup.getLoc(), "fixup value must be 4-byte aligned");
+    return (Value & 0xffff) << 8;
+  }
+}
+
+static unsigned getSize(unsigned Kind) {
+  switch (Kind) {
+  default:
+    return 3;
+  case Xtensa::fixup_xtensa_branch_6:
+    return 2;
+  }
 }
 
 void XtensaMCAsmBackend::applyFixup(const MCAssembler &Asm,
                                     const MCFixup &Fixup, const MCValue &Target,
                                     MutableArrayRef<char> Data, uint64_t Value,
-                                    bool IsResolved) const {}
+                                    bool IsResolved) const {
+  MCFixupKind Kind = Fixup.getKind();
+  MCContext &Ctx = Asm.getContext();
+  Value = adjustFixupValue(Fixup, Value, Ctx);
+
+  if (!Value)
+    return; // Doesn't change encoding.
+
+  unsigned Offset = Fixup.getOffset();
+  unsigned FullSize = getSize(Fixup.getKind());
+
+  for (unsigned i = 0; i != FullSize; ++i) {
+    Data[Offset + i] |= uint8_t((Value >> (i * 8)) & 0xff);
+  }
+}
 
 bool XtensaMCAsmBackend::mayNeedRelaxation(const MCInst &Inst) const {
   return false;
@@ -76,9 +152,30 @@ void XtensaMCAsmBackend::relaxInstruction(const MCInst &Inst,
 
 bool XtensaMCAsmBackend::writeNopData(uint64_t Count,
                                       MCObjectWriter *OW) const {
-  //TODO finish implementation
-  for (uint64_t I = 0; I != Count; ++I)
-    OW->write8(7);
+  uint64_t NumNops = Count / 3;
+  for (uint64_t i = 0; i != NumNops; ++i) {
+    if (IsLittleEndian) {
+      OW->write8(0xf0);
+      OW->write8(0x20);
+      OW->write8(0x00);
+    } else {
+      OW->write8(0x00);
+      OW->write8(0x20);
+      OW->write8(0xf0);
+    }
+  }
+  // TODO maybe function should return error if (Count%3 > 0)
+  switch (Count % 3) {
+  default:
+    break;
+  case 1:
+    OW->write8(0);
+    break;
+  case 2:
+    OW->write16(0);
+    break;
+  }
+
   return true;
 }
 
@@ -90,4 +187,3 @@ MCAsmBackend *llvm::createXtensaMCAsmBackend(const Target &T,
       MCELFObjectTargetWriter::getOSABI(STI.getTargetTriple().getOS());
   return new XtensaMCAsmBackend(OSABI);
 }
-
